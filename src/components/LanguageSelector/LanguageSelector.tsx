e7ca5389baea971b01e8e1eb62fc0658141f4eec
ktung <ptung@protonmail.ch>
2022-12-20 15:19:48 -0500

    fix TS LanguageList

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -16,7 +16,7 @@ export function LanguageSelector() {
     <div>
       {Object.keys(lngs).map((lng) => (
         <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-          {lngs[lng].nativeName}
+          {lngs[lng as keyof LanguageList].nativeName}
         </button>
       ))}
     </div>

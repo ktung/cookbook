3458e10fb053c5c31bacd62a5adfb7353917f9fb
ktung <ptung@protonmail.ch>
2024-12-29 19:37:54 -0500

    update route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  ...prefix("cookbook", [
+    index("routes/home.tsx"),
+    route("*", "routes/home.tsx", { id: "fallback" }),
+  ])
+] satisfies RouteConfig;

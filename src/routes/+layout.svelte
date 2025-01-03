92395b1f6b32bfffff043c0eca63a7457d789faf
ktung <ptung@protonmail.ch>
2025-01-03 11:17:25 -0500

    prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+  export const prerender = true;
+
+  import { ParaglideJS } from '@inlang/paraglide-sveltekit'
+  import { i18n } from '$lib/i18n'
 
   import '../app.css';
   import Navigation from '$lib/components/Navigation.svelte';
